494adaad6da1309631c809d9fd7da4aa52592ca3
raphaelkxy <1277609958@qq.com>
2019-05-12 23:54:47 +0800

    添加消费方和tomcat错误描述

---
+++ src/main/java/com/itheima/demo/service/imp/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.itheima.demo.service.imp;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.itheima.demo.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	public String getName() {
+		// TODO 自动生成的方法存根
+		return "itheima";
+	}
+	
+	
+	
+
+}
