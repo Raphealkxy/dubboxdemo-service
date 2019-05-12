af5d606eb35e9cb04018cc352cfe7fa0e0c4cbbe
raphaelkxy <1277609958@qq.com>
2019-05-13 00:28:30 +0800

    访问通了，但是字符串显示在地址上

--- src/main/java/com/itheima/demo/service/imp/UserServiceImpl.java
+++ src/main/java/com/itheima/demo/service/imp/UserServiceImpl.java
@@ -6,10 +6,13 @@ import com.itheima.demo.service.UserService;
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Override
 	public String getName() {
 		// TODO 自动生成的方法存根
-		return "itheima";
+		return "itheima1";
 	}
+	
+	
 	
 	
 	
